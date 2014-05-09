337b6b6a05102e5dfae4e1e62568969afe7da9f8
Josh Roberts <josh.roberts@live.co.uk>
2014-05-09 21:07:20 +0100

    changes

--- WordIndexer/Haskell/src/Main_2.hs
+++ WordIndexer/Haskell/src/Main_2.hs
@@ -2,6 +2,8 @@ module Main_2 where
 import qualified Data.List as List
 import qualified Data.Char as Char
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 
 main = do
     -- load file to be indexed and stop words
@@ -13,16 +15,16 @@ main = do
     let stopWord = map words (lines (map Char.toLower stopWords))
     
     -- cast both lists to a set to remove duplicates and remove any instances of stopWord from word
-    let index = Set.toList (Set.difference (Set.fromList (concat word)) (Set.fromList (concat stopWord)))
-    
-    -- zip list of words and their line numbers into a tuple and print
-    mapM print (List.zip index (generateIndex index word))
+    let index = Set.toAscList (Set.difference (Set.fromList (concat word)) (Set.fromList (concat stopWord)))
+    -- get list of line numbers for each word in index and print result 
+    mapM print (generateIndex index word)
     
 normalise :: String -> String
 normalise = filter (\x -> Char.isLetter x || Char.isSpace x) . map Char.toLower
-    
-generateIndex :: [String] -> [[String]] -> [[Int]]
-generateIndex xs y = foldr (\ x -> (++) [getLineNumbers x y]) [] xs
+
+generateIndex :: [String] -> [[String]] -> [(String, [Int])]
+generateIndex [] y = []
+generateIndex (x:xs) y = [(x, getLineNumbers x y)] ++ (generateIndex xs y)
 
 getLineNumbers' :: String -> [[String]] -> Int -> [Int]
 getLineNumbers' x [] n = []
